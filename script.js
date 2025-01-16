80441a413928efebb174958b0b6a83484501db56
endercon studio <damisalvatore2011@gmail.com>
2025-01-16 19:28:03 +0000

    New good looking site

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cookieBanner = document.getElementById('cookie-banner');
-    const acceptCookiesButton = document.getElementById('accept-cookies');
-  
-    // Verifica se i cookie sono già stati accettati
-    if (!localStorage.getItem('cookiesAccepted')) {
-      cookieBanner.style.display = 'flex';
-    } else {
-        cookieBanner.style.display = 'none'; // Nasconde il banner se i cookie sono già accettati
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector('.navbar');
+  let lastScrollTop = 0;
 
-
-    // cambiare id di tutti i bottoni in btn e poi su getElemntById cambiare l'id con "btn" così il sound va per tutti i bottoni
-    acceptCookiesButton.addEventListener('click', function() {
-      const audio = new Audio('Sounds/sfx_click.wav'); 
-      audio.play();
-   });
-    
-  
-    // Gestione del click sul pulsante di accettazione
-    acceptCookiesButton.addEventListener('click', function() {
-      localStorage.setItem('cookiesAccepted', 'true');
-      cookieBanner.style.display = 'none';
-    });
+  window.addEventListener('scroll', () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTop > lastScrollTop) {
+          navbar.style.top = '-60px';
+      } else {
+          navbar.style.top = '0';
+      }
+      lastScrollTop = scrollTop;
   });
-
-
-
-  //sono altamente skibidi!1111!!!
-
-  document.getElementById('background-music').volume = 0.5;
+});
